c0fed5f09efd5034856aac89d337d3cb9ac3376e
MartinMwiti <martinmwiti007@gmail.com>
2020-02-22 20:26:19 +0300

    initial commit

--- code.py
+++ code.py
@@ -2,6 +2,7 @@ alpha = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
 
 string_input = input("Enter a string: ")
 # print(string_input)
+shift_input = int(input("Enter in a value to shift by: "))
 
 input_length = len(string_input)
 print(input_length)
@@ -11,7 +12,7 @@ string_output = ""
 for i in range(input_length):
     character = string_input[i].upper()
     location_of_character = alpha.find(character) # returns character index location
-    new_location = location_of_character + 3
+    new_location = location_of_character + shift_input
     string_output += alpha[new_location]
 
 
