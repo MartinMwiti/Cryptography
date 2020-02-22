a170353b986c278135fe25bf58cf7bf8dbcdb07e
MartinMwiti <martinmwiti007@gmail.com>
2020-02-22 20:59:09 +0300

    initial commit2

--- code.py
+++ code.py
@@ -12,9 +12,12 @@ string_output = ""
 for i in range(input_length):
     character = string_input[i].upper()
     location_of_character = alpha.find(character) # returns character index location
-    new_location = location_of_character + shift_input
-    string_output += alpha[new_location]
+    new_location = (location_of_character + shift_input) % 26 # divides by 26 and takes the module value
+    # if new_location >= 26: # long-method
+    #     new_location -= 26
+    string_output += alpha[new_location] # adding '+' concatenate each character. if removed, i.e '=' then all previous characters are over-ridden.
 
 
 print("Encrypted text is: ", string_output.lower())
 
+print(3000 % 26)
