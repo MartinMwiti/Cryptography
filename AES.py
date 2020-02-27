e41c8e47b3bc0c7a28eba10ae6c28f2e917cfc7b
MartinMwiti <martinmwiti007@gmail.com>
2020-02-27 21:06:30 +0300

    encryting plaintext

--- AES.py
+++ AES.py
@@ -3,16 +3,14 @@
 
 from Crypto.Cipher import AES
 
-from Crypto.Cipher import AES
-
 key = b'Sixteen byte key'
 data = b'hello from other side'
 
-e_cipher = AES.new(key, AES.MODE_OCB)
+e_cipher = AES.new(key, AES.MODE_EAX)
 e_data = e_cipher.encrypt(data)
 
 # You need to create a new cipher object after encryption to do the decryption.
-d_cipher = AES.new(key, AES.MODE_OCB, e_cipher.nonce)
+d_cipher = AES.new(key, AES.MODE_EAX, e_cipher.nonce)
 d_data = d_cipher.decrypt(e_data)
 
 print("Encryption was: ", e_data)
