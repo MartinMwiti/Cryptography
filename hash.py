07dc45c54e0b17e67a68b42a0f4397b83c0f5f8d
MartinMwiti <martinmwiti007@gmail.com>
2020-02-28 15:56:28 +0300

    hash

--- hash.py
+++ hash.py
@@ -2,12 +2,10 @@ from cryptography.hazmat.backends import default_backend
 from cryptography.hazmat.primitives import hashes
 import base64
 
-digest = hashes.Hash(hashes.SHA256(), backend=default_backend())
+digest = hashes.Hash(hashes.SHA512(), backend=default_backend()) # The higher the SHA the better and more pythsecure
 digest.update(b"Python is the best programming language!!")
-digest.update(b"Python is the best programming Language!!")
 
-msg_digest = digest.finalize()  # digest of the above two lines
+msg_digest = digest.finalize() 
 
-# multiply by 8 to convert from bytes to bits
 print('msg_digest: ', len(msg_digest), len(msg_digest)*8)
-print('Base64 encoding: ', base64.b64decode(msg_digest + b'='))
+print('Base64 encoding: ', base64.b64decode(msg_digest + b'=='))
